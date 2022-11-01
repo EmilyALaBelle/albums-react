607641e028bbdbaed1e86fdfdafc710446ae0c98
Emily LaBelle <emilylabelle8@gmail.com>
2022-11-01 15:02:28 -0400

    add album

--- src/components/AddAlbum.jsx
+++ src/components/AddAlbum.jsx
@@ -1,4 +1,7 @@
+import { useState } from "react"
+
 export default function AddAlbum() {
+    const [album, setAlbum] = useState('')
     const handleSubmit = (e) => {
         e.preventDefault()
     }
@@ -7,7 +10,9 @@ export default function AddAlbum() {
             <h3>Add new Album</h3>
             <form onSubmit={handleSubmit}>
                 <label htmlFor="album">Album:
-                    <input type="text" name="album" />
+                    <input type="text" name="album" 
+                    onChange = {e => setAlbum(e.target.value)}
+                    value = {album} />
                 </label>
             </form>
         </section>
