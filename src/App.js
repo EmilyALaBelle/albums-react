1cd9eb8fc4bc653900b8419aac964daaa48baaac
Emily LaBelle <emilylabelle8@gmail.com>
2022-11-01 13:55:28 -0400

    album list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/Header';
+import AlbumList from './components/AlbumList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    <Header />
+      <Header />
+      <AlbumList />
     </div>
   );
 }
