e8784f68c470345e8cfbca80eb2ed446930bc0ec
Emily LaBelle <emilylabelle8@gmail.com>
2022-11-01 15:08:42 -0400

    add album

--- src/components/AddAlbum.jsx
+++ src/components/AddAlbum.jsx
@@ -2,6 +2,8 @@ import { useState } from "react"
 
 export default function AddAlbum() {
     const [album, setAlbum] = useState('')
+    const [artist, setArtist] = useState('')
+    const [year, setYear] = useState()
     const handleSubmit = (e) => {
         e.preventDefault()
     }
@@ -10,10 +12,22 @@ export default function AddAlbum() {
             <h3>Add new Album</h3>
             <form onSubmit={handleSubmit}>
                 <label htmlFor="album">Album:
-                    <input type="text" name="album" 
-                    onChange = {e => setAlbum(e.target.value)}
-                    value = {album} />
+                    <input type="text" name="album"
+                        onChange={e => setAlbum(e.target.value)}
+                        value={album} />
                 </label>
+                <br />
+                <label htmlFor="artist">Artist:
+                    <input type="text" name="artist"
+                        onChange={e => setArtist(e.target.value)}
+                        value={artist} /></label>
+                <br />
+                <label htmlFor="year">Year:
+                    <input type="number" name="year"
+                        onChange={e => setYear(e.target.value)}
+                        value={year} /></label>
+                        <br />
+                        <input type="submit" value='Add Album' />
             </form>
         </section>
     )
